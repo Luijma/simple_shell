12998c6fb5bd0e59f3806b1eff15122ef7e0e294
Luijma <rsluismanuel@gmail.com>
2021-08-19 22:51:42 +0000

    Creation of initial files for the simple shell project

---
+++ holberton.h
@@ -0,0 +1,8 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <unistd.h>
+
+#endif /* _HOLBERTON_H_
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ * main - entry point for simple_shell program
+ * Return: 0 on success
+ */
+int main(void)
+{
+	return (0);
+}
