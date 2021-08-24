e6c30759cd075c85762b62a64cbbe0e6b29b3ac3
Jose Torres <jose.torres122@outlook.com>
2021-08-24 16:48:37 +0000

    Allow input to be processed

--- processInput.c
+++ processInput.c
@@ -1,4 +1,21 @@
 #include "holberton.h"
+
+/**
+ * Take_Input - gets input from the user
+ * Return: returns user input
+ */
+char *Take_Input(void)
+{
+	char *input = NULL;
+	int length = 0;
+	size_t bufsize = 0;
+
+	length = getline(&input, &bufsize, stdin);
+	input[length - 1] = '\0';
+
+	return (input);
+}
+
 /**
  * Process_Input - takes input string, locates spaces
  * in the string and creates an array of strings
@@ -7,35 +24,30 @@
  */
 char **Process_Input(char *input)
 {
-	/*
-	const char sep = " ";
-	char *token; */
-	/* Placeholder Code */
-	char **args = malloc(4 * sizeof(char*));
+	char *token;
+	char **input_array;
+	int idx, length;
+	int counter = 0;
 
-	args[0] = "/bin/sh";
-	args[1] = input;
-	args[2] = NULL;
-	
-
-	return (args);
-}
-/**
- * Take_Input - gets input from the user
- * Return: returns user input
- */
-char *Take_Input()
-{
-	char *input = NULL;
-	ssize_t bufsize = 0;
-
-	input = malloc(bufsize * sizeof(char));
-	if (!input)
+	for (idx = 0 ; input[idx] != '\0' ; idx++)
 	{
-		printf("Memory Allocation Error!\n");
-		exit(EXIT_FAILURE);
+		if (input[idx] != ' ')
+			counter++;
 	}
-	getline(&input, &bufsize, stdin);
 
-	return (input);
+	input_array = malloc(sizeof(char) * counter);
+
+	token = strtok(input, " ");
+
+	for (idx = 0 ; token != NULL ; idx++)
+	{
+		length = strlen(token);
+		input_array[idx] = malloc(sizeof(char) * length);
+
+		input_array[idx] = token;
+		token = strtok(NULL, " ");
+	}
+	input_array[idx + 1] = NULL;
+
+	return (input_array);
 }
